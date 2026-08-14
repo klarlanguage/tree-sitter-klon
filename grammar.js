2ca134ce59cc85873b3ebf9761b74723b939824f
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-08-14 17:41:55 -0500

    Add Klon configuration to Zed extension

--- grammar.js
+++ grammar.js
@@ -1,3 +1,4 @@
+// oxlint-disable no-unused-vars
 /**
  * @file Klon grammar for tree-sitter
  * @author ProCode Software
@@ -7,6 +8,9 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+const unicodeDigit = /\p{N}/u
+const letter = /[\p{L}_]/u
+
 export default grammar({
     name: 'klon',
     extras: $ => [/\s/, $.comment],
@@ -15,5 +19,11 @@ export default grammar({
         source_file: $ => 'hello',
         comment: $ =>
             token(choice(seq('//', /.*/), seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/'))),
+
+        int_literal: $ => choice($.hex_literal, $.decimal_literal, $.binary_literal),
+        hex_literal: () => /0x[A-Fa-f0-9_]+/,
+        binary_literal: () => /0b[0-1_]+/,
+        decimal_literal: () => /[0-9_]+/,
+        // TODO: Float
     },
 })
